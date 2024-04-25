de94f32a3afa4995af6788f87a3dde7c1fb7bf21
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-04-25 20:08:36 +0200

    talll

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication
-    ){
-        
-    }
+//fun main() = application {
+//    Window(onCloseRequest = ::exitApplication) {
+//        tutorial01()
+//    }
+//}
+
+
+fun main() {
+    tutorial01()
 }
